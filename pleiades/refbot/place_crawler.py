5a76a0646b6cf3329f512ca0868ea8b11f0df23d
Tom Elliott <tom.elliott@nyu.edu>
2018-03-14 08:08:37 -0500

    Stub out PlaceCrawler class and set up module-level logger. Tests pass.

--- pleiades/refbot/place_crawler.py
+++ pleiades/refbot/place_crawler.py
@@ -3,3 +3,12 @@
 """Crawl through Pleiades place resources."""
 
 import logging
+logger = logging.getLogger(__name__)
+
+
+class PlaceCrawler():
+    """A class to crawl a hierarchical directory tree of Pleiades place JSON.
+    """
+
+    def __init__(self):
+        """Initialize the PlaceCrawler class. (change me)"""
