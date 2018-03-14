8cf4d0adbbd9f6be64d5a8cbbf86c38ece989b99
Tom Elliott <tom.elliott@nyu.edu>
2018-03-14 08:22:08 -0500

    add counting by crawler. tests pass

--- pleiades/refbot/place_crawler.py
+++ pleiades/refbot/place_crawler.py
@@ -3,6 +3,9 @@
 """Crawl through Pleiades place resources."""
 
 import logging
+from os import walk
+from os.path import abspath, isdir, realpath
+
 logger = logging.getLogger(__name__)
 
 
@@ -10,5 +13,22 @@ class PlaceCrawler():
     """A class to crawl a hierarchical directory tree of Pleiades place JSON.
     """
 
-    def __init__(self):
-        """Initialize the PlaceCrawler class. (change me)"""
+    def __init__(self, json_path, sniff=True):
+        """Initialize the PlaceCrawler class and sniff the JSON tree."""
+        self.json_path = abspath(realpath(json_path))
+        if sniff:
+            if not isdir(self.json_path):
+                raise IOError(
+                    '{} is not a valid directory path'.format(self.json_path))
+            self._count_json_files()
+        else:
+            self.count = -1
+
+    def _count_json_files(self):
+        """Walk the tree and json_path and count each JSON file visited."""
+        self.count = 0
+        for root, dirs, files in walk(self.json_path):
+            logger.debug('at {}: {}'.format(root, repr(files)))
+            json_files = [f for f in files if f.endswith('.json')]
+            logger.debug('json_files: {}'.format(repr(json_files)))
+            self.count += len(json_files)
