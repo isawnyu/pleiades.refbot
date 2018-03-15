48676e7ac3501d8ea32fa8c0906dacc20070a26a
Tom Elliott <tom.elliott@nyu.edu>
2018-03-15 13:28:01 -0500

    stub out zotero support and tests

---
+++ tests/test_zotero.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Test Zotero support for pleiades.refbot"""
+
+import logging
+from nose.tools import assert_equal, assert_false, assert_true, raises
+from os.path import abspath, join, realpath
+from unittest import TestCase
+
+logger = logging.getLogger(__name__)
+test_data_path = ['tests', 'data']
+
+
+def setup_module():
+    """Change me"""
+    pass
+
+
+def teardown_module():
+    """Change me"""
+    pass
+
+
+class Test_This(TestCase):
+
+    def setUp(self):
+        """Change me"""
+        pass
+
+    def tearDown(self):
+        """Change me"""
+        pass
+
+    def test_a(self):
+        """Change me"""
+        pass
